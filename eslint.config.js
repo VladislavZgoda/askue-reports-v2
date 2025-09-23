73f4ccb7cb7e69c6bc5aea95a253d4d8b7334e12
zgodavg <www.vlad78@yandex.ru>
2025-09-23 10:48:07 +0300

    Setup @tanstack/eslint-plugin-router

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import pluginRouter from "@tanstack/eslint-plugin-router";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 import { defineConfig, globalIgnores } from "eslint/config";
 
@@ -16,6 +17,7 @@ export default defineConfig([
       tseslint.configs.stylisticTypeChecked,
       reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
+      ...pluginRouter.configs["flat/recommended"],
       eslintConfigPrettier,
     ],
     languageOptions: {
