3344948aa56ca1787821849915352d02c6b52e46
zgodavg <www.vlad78@yandex.ru>
2025-09-24 08:57:13 +0300

    Add react plugins to eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,8 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import reactX from "eslint-plugin-react-x";
+import reactDom from "eslint-plugin-react-dom";
 import pluginRouter from "@tanstack/eslint-plugin-router";
 import pluginQuery from "@tanstack/eslint-plugin-query";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
@@ -18,6 +20,8 @@ export default defineConfig([
       tseslint.configs.stylisticTypeChecked,
       reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
+      reactX.configs["recommended-typescript"],
+      reactDom.configs.recommended,
       ...pluginRouter.configs["flat/recommended"],
       ...pluginQuery.configs["flat/recommended"],
       eslintConfigPrettier,
@@ -25,7 +29,9 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: tseslint.parser,
       parserOptions: {
+        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
