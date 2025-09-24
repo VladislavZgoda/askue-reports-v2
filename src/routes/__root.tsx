d86d6648b0ba5bcba0943cad4aea297940eb7928
Garry3Fingers <www.vlad78@yandex.ru>
2025-09-24 22:16:11 +0300

    Setup @tanstack/react-start

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,32 +1,103 @@
+/// <reference types="vite/client" />
 import {
-  createRootRouteWithContext,
+  HeadContent,
   Link,
   Outlet,
+  Scripts,
+  createRootRouteWithContext,
 } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { DefaultCatchBoundary } from "~/components/DefaultCatchBoundary";
+import { NotFound } from "~/components/NotFound";
+
+import appCss from "~/styles.css?url";
+
+import type { ReactNode } from "react";
 import type { QueryClient } from "@tanstack/react-query";
 
-const RootLayout = () => (
-  <>
-    <div className="p-2 flex gap-2">
-      <Link to="/" className="[&.active]:font-bold">
-        Home
-      </Link>{" "}
-      <Link to="/about" className="[&.active]:font-bold">
-        About
-      </Link>
-    </div>
-    <hr />
-    <Outlet />
-    <TanStackRouterDevtools position="bottom-right" />
-    <ReactQueryDevtools buttonPosition="bottom-left" />
-  </>
-);
-
-export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
-  {
-    component: RootLayout,
+export const Route = createRootRouteWithContext<{
+  queryClient: QueryClient;
+}>()({
+  head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+      {
+        title: "Отчеты АСКУЭ",
+      },
+    ],
+    links: [
+      { rel: "icon", href: "/favicon.ico" },
+      { rel: "stylesheet", href: appCss },
+    ],
+  }),
+  errorComponent: (props) => {
+    return (
+      <RootDocument>
+        <DefaultCatchBoundary {...props} />
+      </RootDocument>
+    );
   },
-);
+  notFoundComponent: () => <NotFound />,
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <RootDocument>
+      <Outlet />
+    </RootDocument>
+  );
+}
+
+function RootDocument({ children }: { children: ReactNode }) {
+  return (
+    <html>
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        <div className="p-2 flex gap-2 text-lg">
+          <Link
+            to="/"
+            activeProps={{
+              className: "font-bold",
+            }}
+            activeOptions={{ exact: true }}
+          >
+            Home
+          </Link>{" "}
+          <Link
+            to="/about"
+            activeProps={{
+              className: "font-bold",
+            }}
+          >
+            About
+          </Link>{" "}
+          <Link
+            // @ts-expect-error: This is for testing.
+            to="/this-route-does-not-exist"
+            activeProps={{
+              className: "font-bold",
+            }}
+          >
+            This Route Does Not Exist
+          </Link>
+        </div>
+        <hr />
+        {children}
+        <TanStackRouterDevtools position="bottom-right" />
+        <ReactQueryDevtools buttonPosition="bottom-left" />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
 import { defineConfig } from "vite";
-import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import viteTsConfigPaths from "vite-tsconfig-paths";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   plugins: [
     tailwindcss(),
-    tanstackRouter({
-      target: "react",
-      autoCodeSplitting: true,
+    viteTsConfigPaths({
+      projects: ["./tsconfig.json"],
     }),
+    tanstackStart(),
     react(),
   ],
 });
