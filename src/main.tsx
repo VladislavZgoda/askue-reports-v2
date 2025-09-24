8080d03e962035df80b39af75eeeda6d59c4bde2
Garry3Fingers <www.vlad78@yandex.ru>
2025-09-24 21:08:07 +0300

    Setup react-query integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,9 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { routeTree } from "./routeTree.gen";
+import { RouterProvider } from "@tanstack/react-router";
+import { createAppRouter } from "./router";
 
-const queryClient = new QueryClient();
-
-const router = createRouter({
-  routeTree,
-  context: {
-    queryClient,
-  },
-  defaultPreload: "intent",
-  // Since we're using React Query, we don't want loader calls to ever be stale
-  // This will ensure that the loader is always called when the route is preloaded or visited
-  defaultPreloadStaleTime: 0,
-  scrollRestoration: true,
-});
+const router = createAppRouter();
 
 declare module "@tanstack/react-router" {
   interface Register {
@@ -31,10 +18,7 @@ if (!rootElement.innerHTML) {
 
   root.render(
     <StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
-      ,
+      <RouterProvider router={router} />
     </StrictMode>,
   );
 }
