c2bfd93dca0674d10b9d7e81add08b64399b4bc5
zgodavg <www.vlad78@yandex.ru>
2025-09-26 15:19:26 +0300

    Refactor code

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import * as schema from "./schema";
 import postgres from "postgres";
 
-const client = postgres(process.env.DATABASE_URL!);
+const client = postgres(process.env.DATABASE_URL);
 
 export const db = drizzle(client, {
   schema,
--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -2,7 +2,7 @@ import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 
-const migrationClient = postgres(process.env.DATABASE_URL!, {
+const migrationClient = postgres(process.env.DATABASE_URL, {
   max: 1,
 });
 
