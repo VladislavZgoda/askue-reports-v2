914938d8c08566f5b53910aa41b9d869737422a5
zgodavg <www.vlad78@yandex.ru>
2025-09-26 09:26:32 +0300

    Setup drizzle orm

---
+++ src/db/migrate.ts
@@ -0,0 +1,17 @@
+import postgres from "postgres";
+import { drizzle } from "drizzle-orm/postgres-js";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+
+const migrationClient = postgres(process.env.DATABASE_URL!, {
+  max: 1,
+});
+
+async function runMigration() {
+  await migrate(drizzle(migrationClient), {
+    migrationsFolder: "src/db/migrations",
+  });
+
+  await migrationClient.end();
+}
+
+await runMigration();
