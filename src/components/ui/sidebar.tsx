947fabad7b5effa3f484cc1b15443e7ce2fbfcbf
Vladislav <www.vlad78@yandex.ru>
2025-10-04 17:24:39 +0300

    Delete unused import

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva } from "class-variance-authority";
-import { PanelLeftIcon, PanelLeftClose, PanelLeftOpen } from "lucide-react";
+import { PanelLeftClose, PanelLeftOpen } from "lucide-react";
 
 import type { VariantProps } from "class-variance-authority";
 
