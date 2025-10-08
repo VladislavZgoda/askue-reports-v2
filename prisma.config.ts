9c9afe16b7c8612b17581dc587f5d3bf821fe7e6
zgodavg <www.vlad78@yandex.ru>
2025-10-08 15:16:35 +0300

    Move prisma folder

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,9 +2,9 @@ import "dotenv/config";
 import type { PrismaConfig } from "prisma";
 
 export default {
-  schema: "./src/prisma/schema.prisma",
+  schema: "./src/server/prisma/schema.prisma",
   migrations: {
-    path: "./src/prisma/migrations",
-    seed: "node --import=tsx ./src/prisma/seed.ts",
+    path: "./src/server/prisma/migrations",
+    seed: "node --import=tsx ./src/server/prisma/seed.ts",
   },
 } satisfies PrismaConfig;
