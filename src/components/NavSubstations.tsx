62c5305f84408f0df4cb7c8cedcf70886b31ca4d
zgodavg <www.vlad78@yandex.ru>
2025-10-10 14:38:03 +0300

    Refactor active state for the Link

--- src/components/NavSubstations.tsx
+++ src/components/NavSubstations.tsx
@@ -1,6 +1,5 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { Link } from "@tanstack/react-router";
-import { useState } from "react";
 import { substationsQueryOptions } from "~/server/services/transformer-substation";
 
 import { HousePlug } from "lucide-react";
@@ -8,9 +7,6 @@ import { SidebarMenu, SidebarMenuItem, SidebarMenuButton } from "./ui/sidebar";
 
 export function NavSubstations() {
   const { data } = useSuspenseQuery(substationsQueryOptions());
-  const [activeLinkId, setActiveLinkId] = useState<number | null>(null);
-
-  const handleLinkClick = (id: number) => setActiveLinkId(id);
 
   if (data.length === 0) {
     return <>Список пуст</>;
@@ -20,11 +16,13 @@ export function NavSubstations() {
     <SidebarMenu>
       {data.map(({ id, name }) => (
         <SidebarMenuItem key={id}>
-          <SidebarMenuButton asChild isActive={id === activeLinkId}>
+          <SidebarMenuButton asChild>
             <Link
               to="/substations/$substationId"
               params={{ substationId: id.toString() }}
-              onClick={() => handleLinkClick(id)}
+              activeProps={{
+                className: "bg-sidebar-accent text-sidebar-accent-foreground",
+              }}
             >
               <HousePlug />
               <span>{name}</span>
