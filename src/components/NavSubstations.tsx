43c16438fd8c0c49cdad3dbec8007024c29440d3
zgodavg <www.vlad78@yandex.ru>
2025-10-10 13:34:15 +0300

    Implement navigation

--- src/components/NavSubstations.tsx
+++ src/components/NavSubstations.tsx
@@ -1,4 +1,6 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { Link } from "@tanstack/react-router";
+import { useState } from "react";
 import { substationsQueryOptions } from "~/server/services/transformer-substation";
 
 import { HousePlug } from "lucide-react";
@@ -6,6 +8,9 @@ import { SidebarMenu, SidebarMenuItem, SidebarMenuButton } from "./ui/sidebar";
 
 export function NavSubstations() {
   const { data } = useSuspenseQuery(substationsQueryOptions());
+  const [activeLinkId, setActiveLinkId] = useState<number | null>(null);
+
+  const handleLinkClick = (id: number) => setActiveLinkId(id);
 
   if (data.length === 0) {
     return <>Список пуст</>;
@@ -13,13 +18,17 @@ export function NavSubstations() {
 
   return (
     <SidebarMenu>
-      {data.map((substation) => (
-        <SidebarMenuItem key={substation.id}>
-          <SidebarMenuButton asChild>
-            <a href="#">
+      {data.map(({ id, name }) => (
+        <SidebarMenuItem key={id}>
+          <SidebarMenuButton asChild isActive={id === activeLinkId}>
+            <Link
+              to="/substations/$substationId"
+              params={{ substationId: id.toString() }}
+              onClick={() => handleLinkClick(id)}
+            >
               <HousePlug />
-              <span>{substation.name}</span>
-            </a>
+              <span>{name}</span>
+            </Link>
           </SidebarMenuButton>
         </SidebarMenuItem>
       ))}
