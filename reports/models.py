005ca05cd75f253c18e8d29565b29c87c7e9e4cd
VladislavZgoda <www.vlad78@yandex.ru>
2026-09-14 15:18:54 +0300

    feat: add Substation model

--- reports/models.py
+++ reports/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Substation(TimeStampedModel):
+    name = models.CharField(max_length=15, unique=True)
+
+    def __str__(self):
+        return self.name
+
